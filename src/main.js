f16fb68bec6693e6affa1e32fb355fba922a6188
Pablo Theissen <github@24p.eu>
2020-02-17 16:34:06 +0100

    Code cleanup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/* root element for changen :root-CSS-variables */
+/* root element for changing :root-CSS-variables */
 let domRoot = document.documentElement;
 /* HSL sliders on top */
 let domSliderHue = document.querySelector("#slider-hue");
@@ -23,14 +23,14 @@ let domCheckboxProMode = document.querySelector("#pro-mode");
 let currSliderShade = 500;
 let proMode = true;
 
-var color = {
+let color = {
     shade: 0,
-    color: function(newHue, newSat, newLtn) {
+    color: function (newHue, newSat, newLtn) {
         this.hue = newHue;
         this.sat = newSat;
         this.ltn = newLtn;
     },
-    updateGrid: function() {
+    updateGrid: function () {
         document.querySelector("#grid span.C" + this.shade).style.left = "calc(" + this.l + "% - var(--radius))";
         document.querySelector("#grid span.C" + this.shade).style.bottom = "calc(" + this.s + "% - var(--radius))";
     },
@@ -38,15 +38,15 @@ var color = {
     set hue(newHue) {
         this.h = newHue;
         domRoot.style.setProperty('--hue' + this.shade, this.h);
-        if(this.shade == currSliderShade) {
-            if(domSliderHue !== document.activeElement) {
+        if (this.shade === currSliderShade) {
+            if (domSliderHue !== document.activeElement) {
                 domSliderHue.value = this.h;
             }
             domRoot.style.setProperty('--hueSlider', this.h);
         }
         let input = document.querySelector("#H" + this.shade + " input");
-        if(input !== null) {
-            input.value=this.h;
+        if (input !== null) {
+            input.value = this.h;
         }
         // console.log("New Hue " + this.shade + ": " + this.h);
     },
@@ -54,15 +54,15 @@ var color = {
     set sat(newSat) {
         this.s = newSat;
         domRoot.style.setProperty('--sat' + this.shade, this.s + "%");
-        if(this.shade == currSliderShade) {
-            if(domSliderSat !== document.activeElement) {
+        if (this.shade === currSliderShade) {
+            if (domSliderSat !== document.activeElement) {
                 domSliderSat.value = this.s;
             }
             domRoot.style.setProperty('--satSlider', this.s + "%");
         }
         let input = document.querySelector("#S" + this.shade + " input");
-        if(input !== null) {
-            input.value=this.s;
+        if (input !== null) {
+            input.value = this.s;
         }
         this.updateGrid();
     },
@@ -70,36 +70,36 @@ var color = {
     set ltn(newLtn) {
         this.l = newLtn;
         domRoot.style.setProperty('--ltn' + this.shade, this.l + "%");
-        if(this.shade == currSliderShade) {
-            if(domSliderLtn !== document.activeElement) {
+        if (this.shade === currSliderShade) {
+            if (domSliderLtn !== document.activeElement) {
                 domSliderLtn.value = this.l;
             }
             domRoot.style.setProperty('--ltnSlider', this.l + "%");
         }
         let input = document.querySelector("#L" + this.shade + " input");
-        if(input !== null) {
-            input.value=this.l;
+        if (input !== null) {
+            input.value = this.l;
         }
         this.updateGrid();
     },
-}
-var color100 = Object.create(color);
+};
+let color100 = Object.create(color);
 color100.shade = 100;
-var color200 = Object.create(color);
+let color200 = Object.create(color);
 color200.shade = 200;
-var color300 = Object.create(color);
+let color300 = Object.create(color);
 color300.shade = 300;
-var color400 = Object.create(color);
+let color400 = Object.create(color);
 color400.shade = 400;
-var color500 = Object.create(color);
+let color500 = Object.create(color);
 color500.shade = 500;
-var color600 = Object.create(color);
+let color600 = Object.create(color);
 color600.shade = 600;
-var color700 = Object.create(color);
+let color700 = Object.create(color);
 color700.shade = 700;
-var color800 = Object.create(color);
+let color800 = Object.create(color);
 color800.shade = 800;
-var color900 = Object.create(color);
+let color900 = Object.create(color);
 color900.shade = 900;
 
 /**
@@ -107,25 +107,27 @@ color900.shade = 900;
  * If pro mode is activated, the custom light and dark shades are fetched, too.
  * Otherwise these shades are calculated automatically.
  */
-recalcColors = function() {
-    h500 = parseFloat(document.querySelector("#H500 input").value);
-    s500 = parseFloat(document.querySelector("#S500 input").value);
-    l500 = parseFloat(document.querySelector("#L500 input").value);
-    if(proMode) {
+recalculateColors = function () {
+    let h100 = null;
+    let s100 = null;
+    let l100 = null;
+    let h500 = parseFloat(document.querySelector("#H500 input").value);
+    let s500 = parseFloat(document.querySelector("#S500 input").value);
+    let l500 = parseFloat(document.querySelector("#L500 input").value);
+    let h900 = null;
+    let s900 = null;
+    let l900 = null;
+    if (proMode) {
         h100 = parseFloat(document.querySelector("#H100 input").value);
         s100 = parseFloat(document.querySelector("#S100 input").value);
         l100 = parseFloat(document.querySelector("#L100 input").value);
         h900 = parseFloat(document.querySelector("#H900 input").value);
         s900 = parseFloat(document.querySelector("#S900 input").value);
         l900 = parseFloat(document.querySelector("#L900 input").value);
-        hues = recalcHue(h500, h100, h900);
-        sats = recalcSat(s500, s100, s900);
-        ltns = recalcLtn(l500, l100, l900);
-    } else {
-        hues = recalcHue(h500);
-        sats = recalcSat(s500);
-        ltns = recalcLtn(l500);
     }
+    let hues = recalculateHue(h500, h100, h900);
+    let sats = recalculateSat(s500, s100, s900);
+    let ltns = recalculateLtn(l500, l100, l900);
     color100.color(hues[0], sats[0], ltns[0]);
     color200.color(hues[1], sats[1], ltns[1]);
     color300.color(hues[2], sats[2], ltns[2]);
@@ -140,116 +142,121 @@ recalcColors = function() {
 /**
  * Calculate hue values given either the medium (500) hue or light (100), medium
  * (500), and dark (900) hue.
- * 
+ *
  * If the third parameter is empty or null, the light (100) and dark (900) shades
  * are calculated by subtracting/adding 10.
- * 
- * The inbetween-values (200, 300, 400, 600, 700, 800) are interpolated linearly.
- * 
- * @param {float} h500 Hue value of medium shade: [0-360]
- * @param {?float=} h100 Optional hue value of light shade: [0-360]
- * @param {?float=} h900 Optional hue value of dark shade: [0-360]
- * 
+ *
+ * The inbetween values (200, 300, 400, 600, 700, 800) are interpolated linearly.
+ *
+ * @param {number} h500 Hue value of medium shade: [0-360]
+ * @param {?number=} h100 Optional hue value of light shade: [0-360]
+ * @param {?number=} h900 Optional hue value of dark shade: [0-360]
+ *
  * @return {Array.<float>} hue values for shades 100-900, 0-indexed
  */
-recalcHue = function(h500, h100 = null, h900 = null) {
-    if(h900 == null) {
-        h100 = Math.max(0, h500-10);
-        h900 = Math.min(360, h500+10);
+recalculateHue = function (h500, h100 = null, h900 = null) {
+    if (h900 === null) {
+        h100 = Math.max(0, h500 - 10);
+        h900 = Math.min(360, h500 + 10);
     }
-    
+
     // use linear interpolation
-    return _recalcValue(h100, h500, h900, 1/4, 2/4, 3/4);
+    return _recalculateValue(h100, h500, h900, 0.25, 0.50, 0.75);
 };
 
 /**
  * Calculate saturation values given either the medium (500) saturation or light
  * (100), medium (500), and dark (900) saturation.
- * 
+ *
  * If the third parameter is empty or null, the light (100) shade is calculated as
  * follows:
  * 1. Multiply the medium (500) saturation by 3
  * 2. If the light (100) saturation is larger than 90, use a saturation of 90 instead
  * 3. If the medium (500) saturation is higher than 90, use medium saturation instead
- * 
+ *
  * If the third parameter is empty or null, the dark (900) shade is calculated as
  * follows:
  * 1. Divide the medium (500) saturation by 5
  * 2. If the dark (900) saturation is smaller than 30, use a saturation of 30 instead
  * 3. If the medium (500) saturation is lower than 30, use medium saturation instead
- * 
+ *
  * The inbetween-values (200, 300, 400, 600, 700, 800) are interpolated.
- * 
- * @param {float} s500 Saturation value of medium shade: [1-100]
- * @param {?float=} s100 Optional saturation value of light shade: [1-100]
- * @param {?float=} s900 Optional saturation value of dark shade: [1-100]
- * 
+ *
+ * @param {number} s500 Saturation value of medium shade: [1-100]
+ * @param {?number=} s100 Optional saturation value of light shade: [1-100]
+ * @param {?number=} s900 Optional saturation value of dark shade: [1-100]
+ *
  * @return {Array.<float>} saturation values for shades 100-900, 0-indexed
  */
-recalcSat = function(s500, s100 = null, s900 = null) {
-    if(s900 == null) {
+recalculateSat = function (s500, s100 = null, s900 = null) {
+    if (s900 == null) {
         s100 = Math.max(s500, Math.min(90, 3.0 * s500));
         s900 = Math.min(s500, Math.max(30, 0.2 * s500));
     }
-    
+
     // use non-linear interpolation
-    return _recalcValue(s100, s500, s900, 0.5/4, 1.5/4, 2.8/4);
+    return _recalculateValue(s100, s500, s900, 0.5 / 4, 1.5 / 4, 2.8 / 4);
 };
 
 /**
  * Calculate lightness values given either the medium (500) lightness or light
  * (100), medium (500), and dark (900) lightness.
- * 
+ *
  * If the third parameter is empty or null, the light (100) shade is calculated as
  * follows:
  * 1. Multiply the medium (500) lightness by 3
  * 2. If the light (100) lightness is larger than 95, use a lightness of 95 instead
  * 3. If the medium (500) lightness is higher than 95, use medium lightness instead
- * 
+ *
  * If the third parameter is empty or null, the dark (900) shade is calculated as
  * follows:
  * 1. Divide the medium (500) lightness by 5
  * 2. If the dark (900) lightness is smaller than 30, use a lightness of 30 instead
  * 3. If the medium (500) lightness is lower than 30, use medium lightness instead
- * 
+ *
  * The inbetween-values (200, 300, 400, 600, 700, 800) are interpolated.
- * 
- * @param {float} s500 lightness value of medium shade: [1-100]
- * @param {?float=} s100 Optional lightness value of light shade: [1-100]
- * @param {?float=} s900 Optional lightness value of dark shade: [1-100]
- * 
+ *
+ * @param {number} l500 lightness value of medium shade: [1-100]
+ * @param {?number=} l100 Optional lightness value of light shade: [1-100]
+ * @param {?number=} l900 Optional lightness value of dark shade: [1-100]
+ *
  * @return {Array.<float>} lightness values for shades 100-900, 0-indexed
  */
-recalcLtn = function(l500, l100 = null, l900 = null) {
-    if(l900 == null) {
+recalculateLtn = function (l500, l100 = null, l900 = null) {
+    if (l900 == null) {
         l100 = Math.max(l500, Math.min(95, 3.0 * l500));
         l900 = Math.min(l500, Math.max(10, 0.2 * l500));
     }
-    
+
     // use non-linear interpolation
-    return _recalcValue(l100, l500, l900, 1.2/4, 2.4/4, 3.4/4);
+    return _recalculateValue(l100, l500, l900, 1.2 / 4, 2.4 / 4, 3.4 / 4);
 };
 
 /**
  * Given the light, medium, and dark value of either H, S, or L, calculate the six
- * values (200, 300, 400, 600, 700, 800) inbetween.
- * 
- * Using the factors 0.25, 0.50, and 0.75 a linear interpolation is returend.
- * 
+ * values inbetween (200, 300, 400, 600, 700, 800).
+ *
+ * Using the factors 0.25, 0.50, and 0.75 a linear interpolation is returned.
+ *
  * For a continuous curve, use 0 < factor1 < factor2 < factor3 < 1.
- * 
+ *
  * @param {float} v100 light H, S, or L value
  * @param {float} v500 medium H, S, or L value
  * @param {float} v900 dark H, S, or L value
- * @param {float} factor1 Factor for calculating 400 and 600
- * @param {float} factor2 Factor for calculating 300 and 700
- * @param {float} factor3 Factor for calculating 200 and 800
- * 
+ * @param {number} factor1 Factor for calculating 400 and 600
+ * @param {number} factor2 Factor for calculating 300 and 700
+ * @param {number} factor3 Factor for calculating 200 and 800
+ *
  * @return {Array.<float>} H, S, or L values for shades 100-900, 0-indexed
  */
-_recalcValue = function(v100, v500, v900, factor1, factor2, factor3) {
+_recalculateValue = function (v100,
+                              v500,
+                              v900,
+                              factor1 = 0.25,
+                              factor2 = 0.50,
+                              factor3 = 0.75) {
     let v = [];
-    
+
     v[0] = v100;
     v[1] = (v500 - (v500 - v100) * factor3).toFixed(1);
     v[2] = (v500 - (v500 - v100) * factor2).toFixed(1);
@@ -259,20 +266,20 @@ _recalcValue = function(v100, v500, v900, factor1, factor2, factor3) {
     v[6] = (v500 - (v500 - v900) * factor2).toFixed(1);
     v[7] = (v500 - (v500 - v900) * factor3).toFixed(1);
     v[8] = v900;
-    
-    return v;
-}
 
-changeSliderShade = function(shade) {
+    return v;
+};
+
+changeSliderShade = function (shade) {
     currSliderShade = shade;
     document.querySelector("#color-selector").classList.remove("selected100");
     document.querySelector("#color-selector").classList.remove("selected500");
     document.querySelector("#color-selector").classList.remove("selected900");
     document.querySelector("#color-selector").classList.add("selected" + shade);
-    recalcColors();
+    recalculateColors();
 };
 
-enableProMode = function() {
+enableProMode = function () {
     domCheckboxProMode.checked = "checked";
     domInputH100.disabled = "";
     domInputS100.disabled = "";
@@ -283,7 +290,7 @@ enableProMode = function() {
     proMode = true;
 };
 
-disableProMode = function() {
+disableProMode = function () {
     domCheckboxProMode.checked = "";
     domInputH100.disabled = "disabled";
     domInputS100.disabled = "disabled";
@@ -293,11 +300,11 @@ disableProMode = function() {
     domInputL900.disabled = "disabled";
     changeSliderShade(500);
     proMode = false;
-    recalcColors();
+    recalculateColors();
 };
 
-checkProMode = function() {
-    if(domCheckboxProMode.checked) {
+checkProMode = function () {
+    if (domCheckboxProMode.checked) {
         enableProMode();
     } else {
         disableProMode();
@@ -308,77 +315,66 @@ let demoColorsHue = [40, 20, 0, 304, 203, 151, 15];
 // hues for Signal... RAL colors: https://de.wikipedia.org/wiki/RAL-Farbe
 color500.color(demoColorsHue[Math.floor(Math.random() * demoColorsHue.length)], 60, 50);
 disableProMode();
-recalcColors();
+recalculateColors();
 
-domSliderHue.addEventListener("input", function() {
+domSliderHue.addEventListener("input", function () {
     window['color' + currSliderShade].hue = parseFloat(this.value);
-    recalcColors();
+    recalculateColors();
 });
-domSliderSat.addEventListener("input", function() {
+domSliderSat.addEventListener("input", function () {
     window['color' + currSliderShade].sat = parseFloat(this.value);
-    recalcColors();
+    recalculateColors();
 });
-domSliderLtn.addEventListener("input", function() {
+domSliderLtn.addEventListener("input", function () {
     window['color' + currSliderShade].ltn = parseFloat(this.value);
-    recalcColors();
+    recalculateColors();
 });
-domInputH500.addEventListener("input", function() {
+domInputH500.addEventListener("input", function () {
     color500.hue = parseFloat(this.value);
-    // changeSliderShade(500);
-    recalcColors();
+    recalculateColors();
 });
-domInputS500.addEventListener("input", function() {
+domInputS500.addEventListener("input", function () {
     color500.sat = parseFloat(this.value);
-    // changeSliderShade(500);
-    recalcColors();
+    recalculateColors();
 });
-domInputL500.addEventListener("input", function() {
+domInputL500.addEventListener("input", function () {
     color500.ltn = parseFloat(this.value);
-    changeSliderShade(500);
-    recalcColors();
+    recalculateColors();
 });
-domInputH100.addEventListener("input", function() {
+domInputH100.addEventListener("input", function () {
     color100.hue = parseFloat(this.value);
-    // changeSliderShade(100);
-    recalcColors();
+    recalculateColors();
 });
-domInputS100.addEventListener("input", function() {
+domInputS100.addEventListener("input", function () {
     color100.sat = parseFloat(this.value);
-    // changeSliderShade(100);
-    recalcColors();
+    recalculateColors();
 });
-domInputL100.addEventListener("input", function() {
+domInputL100.addEventListener("input", function () {
     color100.ltn = parseFloat(this.value);
-    // changeSliderShade(100);
-    recalcColors();
+    recalculateColors();
 });
-domInputH100.addEventListener("focus", function() {
+
+let changeSliderShadeTo100 = function () {
     changeSliderShade(100);
-});
-domInputS100.addEventListener("focus", function() {
-    changeSliderShade(100);
-});
-domInputL100.addEventListener("focus", function() {
-    changeSliderShade(100);
-});
-domInputH500.addEventListener("focus", function() {
+};
+domInputH100.addEventListener("focus", changeSliderShadeTo100);
+domInputS100.addEventListener("focus", changeSliderShadeTo100);
+domInputL100.addEventListener("focus", changeSliderShadeTo100);
+
+let changeSliderShadeTo500 = function () {
     changeSliderShade(500);
-});
-domInputS500.addEventListener("focus", function() {
-    changeSliderShade(500);
-});
-domInputL500.addEventListener("focus", function() {
-    changeSliderShade(500);
-});
-domInputH900.addEventListener("focus", function() {
+};
+domInputH500.addEventListener("focus", changeSliderShadeTo500);
+domInputS500.addEventListener("focus", changeSliderShadeTo500);
+domInputL500.addEventListener("focus", changeSliderShadeTo500);
+
+let changeSliderShadeTo900 = function () {
     changeSliderShade(900);
-});
-domInputS900.addEventListener("focus", function() {
-    changeSliderShade(900);
-});
-domInputL900.addEventListener("focus", function() {
-    changeSliderShade(900);
-});
-domCheckboxProMode.addEventListener("change", function() {
+};
+domInputH900.addEventListener("focus", changeSliderShadeTo900);
+domInputS900.addEventListener("focus", changeSliderShadeTo900);
+domInputL900.addEventListener("focus", changeSliderShadeTo900);
+
+domCheckboxProMode.addEventListener("change", function () {
     checkProMode();
 });
