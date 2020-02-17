8651a691b36f1fcd46cbe58e78a3d119bd5e9556
Pablo Theissen <github@24p.eu>
2020-02-17 16:05:15 +0100

    Add docblocks and other comments, remove unused functions

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
+/* root element for changen :root-CSS-variables */
 let domRoot = document.documentElement;
+/* HSL sliders on top */
 let domSliderHue = document.querySelector("#slider-hue");
 let domSliderSat = document.querySelector("#slider-sat");
 let domSliderLtn = document.querySelector("#slider-ltn");
+/* HSL input fields for light (100) color */
 let domInputH100 = document.querySelector("#H100 input");
 let domInputS100 = document.querySelector("#S100 input");
 let domInputL100 = document.querySelector("#L100 input");
+/* HSL input fields for medium (500) color */
 let domInputH500 = document.querySelector("#H500 input");
 let domInputS500 = document.querySelector("#S500 input");
 let domInputL500 = document.querySelector("#L500 input");
+/* HSL input fields for dark (900) color */
 let domInputH900 = document.querySelector("#H900 input");
 let domInputS900 = document.querySelector("#S900 input");
 let domInputL900 = document.querySelector("#L900 input");
+/* checkbox for activating pro mode, letting user define custom light and dark shades */
 let domCheckboxProMode = document.querySelector("#pro-mode");
 
+/* shade of color, the sliders currently manipulate; change only with changeSliderShade() */
 let currSliderShade = 500;
 let proMode = true;
 
@@ -95,6 +102,11 @@ color800.shade = 800;
 var color900 = Object.create(color);
 color900.shade = 900;
 
+/**
+ * Load HSL values from input fields and calculate inbetween colors
+ * If pro mode is activated, the custom light and dark shades are fetched, too.
+ * Otherwise these shades are calculated automatically.
+ */
 recalcColors = function() {
     h500 = parseFloat(document.querySelector("#H500 input").value);
     s500 = parseFloat(document.querySelector("#S500 input").value);
@@ -124,6 +136,22 @@ recalcColors = function() {
     color800.color(hues[7], sats[7], ltns[7]);
     color900.color(hues[8], sats[8], ltns[8]);
 };
+
+/**
+ * Calculate hue values given either the medium (500) hue or light (100), medium
+ * (500), and dark (900) hue.
+ * 
+ * If the third parameter is empty or null, the light (100) and dark (900) shades
+ * are calculated by subtracting/adding 10.
+ * 
+ * The inbetween-values (200, 300, 400, 600, 700, 800) are interpolated linearly.
+ * 
+ * @param {float} h500 Hue value of medium shade: [0-360]
+ * @param {?float=} h100 Optional hue value of light shade: [0-360]
+ * @param {?float=} h900 Optional hue value of dark shade: [0-360]
+ * 
+ * @return {Array.<float>} hue values for shades 100-900, 0-indexed
+ */
 recalcHue = function(h500, h100 = null, h900 = null) {
     if(h900 == null) {
         h100 = Math.max(0, h500-10);
@@ -131,9 +159,33 @@ recalcHue = function(h500, h100 = null, h900 = null) {
     }
     
     // use linear interpolation
-    return recalcValue(h100, h500, h900, 1/4, 2/4, 3/4);
+    return _recalcValue(h100, h500, h900, 1/4, 2/4, 3/4);
 };
 
+/**
+ * Calculate saturation values given either the medium (500) saturation or light
+ * (100), medium (500), and dark (900) saturation.
+ * 
+ * If the third parameter is empty or null, the light (100) shade is calculated as
+ * follows:
+ * 1. Multiply the medium (500) saturation by 3
+ * 2. If the light (100) saturation is larger than 90, use a saturation of 90 instead
+ * 3. If the medium (500) saturation is higher than 90, use medium saturation instead
+ * 
+ * If the third parameter is empty or null, the dark (900) shade is calculated as
+ * follows:
+ * 1. Divide the medium (500) saturation by 5
+ * 2. If the dark (900) saturation is smaller than 30, use a saturation of 30 instead
+ * 3. If the medium (500) saturation is lower than 30, use medium saturation instead
+ * 
+ * The inbetween-values (200, 300, 400, 600, 700, 800) are interpolated.
+ * 
+ * @param {float} s500 Saturation value of medium shade: [1-100]
+ * @param {?float=} s100 Optional saturation value of light shade: [1-100]
+ * @param {?float=} s900 Optional saturation value of dark shade: [1-100]
+ * 
+ * @return {Array.<float>} saturation values for shades 100-900, 0-indexed
+ */
 recalcSat = function(s500, s100 = null, s900 = null) {
     if(s900 == null) {
         s100 = Math.max(s500, Math.min(90, 3.0 * s500));
@@ -141,9 +193,33 @@ recalcSat = function(s500, s100 = null, s900 = null) {
     }
     
     // use non-linear interpolation
-    return recalcValue(s100, s500, s900, 0.5/4, 1.5/4, 2.8/4);
+    return _recalcValue(s100, s500, s900, 0.5/4, 1.5/4, 2.8/4);
 };
 
+/**
+ * Calculate lightness values given either the medium (500) lightness or light
+ * (100), medium (500), and dark (900) lightness.
+ * 
+ * If the third parameter is empty or null, the light (100) shade is calculated as
+ * follows:
+ * 1. Multiply the medium (500) lightness by 3
+ * 2. If the light (100) lightness is larger than 95, use a lightness of 95 instead
+ * 3. If the medium (500) lightness is higher than 95, use medium lightness instead
+ * 
+ * If the third parameter is empty or null, the dark (900) shade is calculated as
+ * follows:
+ * 1. Divide the medium (500) lightness by 5
+ * 2. If the dark (900) lightness is smaller than 30, use a lightness of 30 instead
+ * 3. If the medium (500) lightness is lower than 30, use medium lightness instead
+ * 
+ * The inbetween-values (200, 300, 400, 600, 700, 800) are interpolated.
+ * 
+ * @param {float} s500 lightness value of medium shade: [1-100]
+ * @param {?float=} s100 Optional lightness value of light shade: [1-100]
+ * @param {?float=} s900 Optional lightness value of dark shade: [1-100]
+ * 
+ * @return {Array.<float>} lightness values for shades 100-900, 0-indexed
+ */
 recalcLtn = function(l500, l100 = null, l900 = null) {
     if(l900 == null) {
         l100 = Math.max(l500, Math.min(95, 3.0 * l500));
@@ -151,13 +227,27 @@ recalcLtn = function(l500, l100 = null, l900 = null) {
     }
     
     // use non-linear interpolation
-    return recalcValue(l100, l500, l900, 1.2/4, 2.4/4, 3.4/4);
+    return _recalcValue(l100, l500, l900, 1.2/4, 2.4/4, 3.4/4);
 };
 
 /**
+ * Given the light, medium, and dark value of either H, S, or L, calculate the six
+ * values (200, 300, 400, 600, 700, 800) inbetween.
  * 
+ * Using the factors 0.25, 0.50, and 0.75 a linear interpolation is returend.
+ * 
+ * For a continuous curve, use 0 < factor1 < factor2 < factor3 < 1.
+ * 
+ * @param {float} v100 light H, S, or L value
+ * @param {float} v500 medium H, S, or L value
+ * @param {float} v900 dark H, S, or L value
+ * @param {float} factor1 Factor for calculating 400 and 600
+ * @param {float} factor2 Factor for calculating 300 and 700
+ * @param {float} factor3 Factor for calculating 200 and 800
+ * 
+ * @return {Array.<float>} H, S, or L values for shades 100-900, 0-indexed
  */
-recalcValue = function(v100, v500, v900, factor1, factor2, factor3) {
+_recalcValue = function(v100, v500, v900, factor1, factor2, factor3) {
     let v = [];
     
     v[0] = v100;
@@ -214,25 +304,11 @@ checkProMode = function() {
     }
 };
 
-// color500.hue = getRandomInt(360);
-// color500.sat = 60;
-// color500.ltn = 50;
-color500.color(getRandomInt(360), 60, 50);
+let demoColorsHue = [40, 20, 0, 304, 203, 151, 15];
+// hues for Signal... RAL colors: https://de.wikipedia.org/wiki/RAL-Farbe
+color500.color(demoColorsHue[Math.floor(Math.random() * demoColorsHue.length)], 60, 50);
 disableProMode();
 recalcColors();
-
-
-function isNumeric(num){
-    return !isNaN(num)
-}
-function round_to_precision(x, precision) {
-    var y = +x + (precision === undefined ? 0.5 : precision/2);
-    return y - (y % (precision === undefined ? 1 : +precision));
-}
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
 
 domSliderHue.addEventListener("input", function() {
     window['color' + currSliderShade].hue = parseFloat(this.value);
